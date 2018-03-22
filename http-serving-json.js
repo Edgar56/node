70e33d6706ba1662002227e1cc94a21ced4ca182
Edgar56 <edgaras.chalilovas@gmail.com>
2018-03-22 22:43:48 +0200

    Ended lessons about HTTP and JSON data

--- http-serving-json.js
+++ http-serving-json.js
@@ -8,13 +8,16 @@ http.createServer((req,serverRes)=>{
 
     if(req.method === 'GET' && req.url === '/posts') {
 
-       // serverRes.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+
 
         https.get(url,(httpRes)=>{
 
             httpRes.on('data', data=> {
                httpRes.setEncoding('utf8');
-                console.log(data);
+
+
+                serverRes.write(data);
+
             });
 
             httpRes.on('end', ()=>{
@@ -26,7 +29,11 @@ http.createServer((req,serverRes)=>{
 
         });
 
+    } else  {
 
+        serverRes.writeHead(404, {'Content-Type': 'text/plain'});
+
+        serverRes.end('404 ERROR, could not find what you were looking for  ')
 
     }
 
