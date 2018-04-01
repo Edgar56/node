eacb40e836df17848c54e7b432def119c87d44f3
Edgar56 <edgaras.chalilovas@gmail.com>
2018-04-01 14:27:03 +0300

    Add files via upload
    Files from laptop

---
+++ file-renaming.js
@@ -0,0 +1,9 @@
+const  fs = require('fs');
+
+//rename files
+
+//fs.renameSync('./newfile2.js','newdir/newfile.js');
+
+//Rename or move directories
+
+fs.renameSync('./newDir/ChildDir','./ParentDir');
---
+++ global.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+var name = 'edgar';
+var newName = name.toUpperCase(name);
+
+console.log(`newName variable is ${newName}`);
+
+console.log(__dirname);
+
+console.log(__filename);
